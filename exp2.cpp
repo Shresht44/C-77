3f41f9aca15e25986dca01077519ea2e15ddf8b6
Shresht44 <ironbee22@gmail.com>
2024-07-29 22:50:07 +0530

    Add files via upload

---
+++ exp2.cpp
@@ -0,0 +1,20 @@
+#shresht23070123077
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int myIntVar;
+    float myFloatVar;
+    double myDoubleVar;
+    char myAlphabet;
+    string myLine;
+    bool decision;
+
+    cout << "Enter Integer" <<endl;
+    cin >> myIntVar;
+    cout << "Value of Variable is " << myIntVar << "Size is " << sizeof(myIntVar) <<endl;
+
+
+
+}
